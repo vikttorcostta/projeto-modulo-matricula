290208b9e121fcb73b54a4b4aff016dc2b947d32
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2024-12-13 09:47:05 -0300

    ajuste no componente header de verde para verde gradiente

--- app-matricula/resources/views/layouts/guest.blade.php
+++ app-matricula/resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class=" min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gradient-to-r from-green-400 to-green-700">
+        <div class=" min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gradient-to-r from-green-400 to-green-800">
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
--- app-matricula/resources/views/layouts/navigation.blade.php
+++ app-matricula/resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-green-700 border-b border-gray-100 p-8">
+<nav x-data="{ open: false }" class="bg-gradient-to-r from-green-800 to-green-400 border-b border-gray-100 p-10 shadow-md">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
